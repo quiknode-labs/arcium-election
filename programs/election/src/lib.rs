592d95ea12c102e3830348586fafc48df0e74826
Mike MacCana <mike.maccana@gmail.com>
2025-11-05 14:35:27 -0500

    Separate keys in Account constraints for readability

--- programs/election/src/lib.rs
+++ programs/election/src/lib.rs
@@ -146,6 +146,7 @@ pub mod election {
     pub struct CreatePoll<'info> {
         #[account(mut)]
         pub payer: Signer<'info>,
+
         #[account(
             init_if_needed,
             space = 9,
@@ -155,48 +156,59 @@ pub mod election {
             address = derive_sign_pda!(),
         )]
         pub sign_pda_account: Account<'info, SignerAccount>,
+
         #[account(
             address = derive_mxe_pda!()
         )]
         pub mxe_account: Account<'info, MXEAccount>,
+
         #[account(
             mut,
             address = derive_mempool_pda!()
         )]
         /// CHECK: mempool_account, checked by the arcium program
         pub mempool_account: UncheckedAccount<'info>,
+
         #[account(
             mut,
             address = derive_execpool_pda!()
         )]
         /// CHECK: executing_pool, checked by the arcium program
         pub executing_pool: UncheckedAccount<'info>,
+
         #[account(
             mut,
             address = derive_comp_pda!(computation_offset)
         )]
         /// CHECK: computation_account, checked by the arcium program.
         pub computation_account: UncheckedAccount<'info>,
+
         #[account(
             address = derive_comp_def_pda!(COMP_DEF_OFFSET_CREATE_POLL)
         )]
         pub comp_def_account: Account<'info, ComputationDefinitionAccount>,
+
         #[account(
             mut,
             address = derive_cluster_pda!(mxe_account)
         )]
         pub cluster_account: Account<'info, Cluster>,
+
         #[account(
             mut,
             address = ARCIUM_FEE_POOL_ACCOUNT_ADDRESS,
         )]
         pub pool_account: Account<'info, FeePool>,
+
         #[account(
             address = ARCIUM_CLOCK_ACCOUNT_ADDRESS,
         )]
         pub clock_account: Account<'info, ClockAccount>,
+
         pub system_program: Program<'info, System>,
+
         pub arcium_program: Program<'info, Arcium>,
+
         #[account(
             init,
             payer = payer,
